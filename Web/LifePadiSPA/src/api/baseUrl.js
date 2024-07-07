7a9d054361488354ab818266417a0e14268d1610
vincent <primeobjective20@gmail.com>
2024-07-07 12:27:55 +0100

    updated base url

--- Web/LifePadiSPA/src/api/baseUrl.js
+++ Web/LifePadiSPA/src/api/baseUrl.js
@@ -1,5 +1,6 @@
 
-const baseUrl = 'https://localhost:7216/api/'
+const baseUrl = 'https://4i2f5way3slxupj3rkqood3wje0txtca.lambda-url.us-east-1.on.aws/api/'
+//'https://localhost:7216/api/'
 //'http://localhost:3500/'
 
 export default baseUrl; 
