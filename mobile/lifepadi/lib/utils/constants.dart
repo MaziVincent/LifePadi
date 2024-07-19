305c7233b023fde4d6b17f2bf017ffea3c3f100c
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-07-19 19:20:45 +0100

    Setup logging

--- mobile/lifepadi/lib/utils/constants.dart
+++ mobile/lifepadi/lib/utils/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 /// #629D03
 const Color kDarkPrimaryColor = Color(0xFF629D03);
@@ -11,3 +12,9 @@ const Color kDarkTextColor = Color(0xFF0F0F0F);
 
 /// #5F5F5F
 const Color kLightTextColor = Color(0xFF5F5F5F);
+
+Logger logger = Logger(
+  printer: PrettyPrinter(
+    methodCount: 0,
+  ),
+);
