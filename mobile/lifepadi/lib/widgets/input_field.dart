4d6d906b71bd9c5a545f8dccfe5fea6b3e35bcf3
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-07-24 14:58:51 +0100

    Change input field to stateless widget to improve performance

--- mobile/lifepadi/lib/widgets/input_field.dart
+++ mobile/lifepadi/lib/widgets/input_field.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lifepadi/utils/constants.dart';
 
-class InputField extends HookWidget {
+class InputField extends StatelessWidget {
   const InputField({
     super.key,
     required this.child,
