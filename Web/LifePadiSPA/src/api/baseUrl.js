f79c0d42cdec7d7669c085fb97a31d765e23cb2d
MaziVincent <georgevincent35@gmail.com>
2024-07-30 17:33:33 +0100

    base url

--- Web/LifePadiSPA/src/api/baseUrl.js
+++ Web/LifePadiSPA/src/api/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://localhost:7216/api/' 
+const baseUrl = "https://4i2f5way3slxupj3rkqood3wje0txtca.lambda-url.us-east-1.on.aws/api/"
 //"https://4i2f5way3slxupj3rkqood3wje0txtca.lambda-url.us-east-1.on.aws/api/"
 //"https://4i2f5way3slxupj3rkqood3wje0txtca.lambda-url.us-east-1.on.aws/api/";
 //'https://localhost:7216/api/'
