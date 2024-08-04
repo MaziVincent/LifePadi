f608b1db551c27e278d00350acd23ce90596913a
charlykso <charlykso141@gmail.com>
2024-08-04 07:48:22 +0100

    also returned vendor when returning delivery

--- Web/Api/DTO/ProductDTO.cs
+++ Web/Api/DTO/ProductDTO.cs
@@ -30,6 +30,7 @@ namespace Api.DTO
         public string? Tag { get; set; }
         public int? CategoryId {get; set;}
         public int? VendorId {get; set;}
+        public VendorDtoLite? Vendor { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
     }
