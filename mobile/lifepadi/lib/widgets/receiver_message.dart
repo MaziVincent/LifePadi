dfd2634a69ee81ce4eca77a177a67b5c98b40ff4
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-08-25 01:44:48 +0100

    Remove redundant receiver's name in each message

--- mobile/lifepadi/lib/widgets/receiver_message.dart
+++ mobile/lifepadi/lib/widgets/receiver_message.dart
@@ -28,55 +28,41 @@ class ReceiverMessage extends StatelessWidget {
         ConstrainedBox(
           constraints: BoxConstraints(maxWidth: 0.7.sw),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-              Text(
-                'John Bayo',
-                style: context.textTheme.bodyMedium?.copyWith(
-                  color: const Color(0xFF0D082C),
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.w500,
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 15.w,
+                  vertical: 10.h,
+                ),
+                decoration: ShapeDecoration(
+                  color: const Color(0xFFF1F7FF),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(10.r),
+                      bottomLeft: Radius.circular(10.r),
+                      bottomRight: Radius.circular(10.r),
+                    ),
+                  ),
+                ),
+                margin: EdgeInsets.only(right: 10.w),
+                child: Text(
+                  content,
+                  style: context.textTheme.bodyLarge?.copyWith(
+                    color: const Color(0xFF0D082B),
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
               ),
               5.verticalSpace,
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 15.w,
-                      vertical: 10.h,
-                    ),
-                    decoration: ShapeDecoration(
-                      color: const Color(0xFFF1F7FF),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(10.r),
-                          bottomLeft: Radius.circular(10.r),
-                          bottomRight: Radius.circular(10.r),
-                        ),
-                      ),
-                    ),
-                    margin: EdgeInsets.only(right: 10.w),
-                    child: Text(
-                      content,
-                      style: context.textTheme.bodyLarge?.copyWith(
-                        color: const Color(0xFF0D082B),
-                        fontSize: 16.sp,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
-                  5.verticalSpace,
-                  Text(
-                    time,
-                    style: GoogleFonts.sourceSans3(
-                      color: const Color(0x660D082C),
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
+              Text(
+                time,
+                style: GoogleFonts.sourceSans3(
+                  color: const Color(0x660D082C),
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w400,
+                ),
               ),
             ],
           ),
