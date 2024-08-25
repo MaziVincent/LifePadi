49622e37b78da455c8082e79f62610b855cf7f94
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-08-25 02:20:36 +0100

    Animate typing loader

--- mobile/lifepadi/lib/widgets/typing_loader.dart
+++ mobile/lifepadi/lib/widgets/typing_loader.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lifepadi/utils/helpers.dart';
 
-class TypingLoader extends StatelessWidget {
+class TypingLoader extends HookWidget {
   const TypingLoader({
     super.key,
     this.color = const Color(0xFFC6DEFF),
@@ -12,18 +13,43 @@ class TypingLoader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final animationController = useAnimationController(
+      duration: const Duration(milliseconds: 1200),
+    )..repeat(reverse: true);
+
+    final animations = List.generate(3, (index) {
+      return Tween<double>(begin: 0.3, end: 1).animate(
+        CurvedAnimation(
+          parent: animationController,
+          curve: Interval(
+            index * 0.2,
+            1,
+            curve: Curves.easeInOut,
+          ),
+        ),
+      );
+    });
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       mainAxisSize: MainAxisSize.min,
       children: [
-        for (final _ in [1, 2, 3])
-          Container(
-            width: 15.h,
-            height: 15.h,
-            decoration: ShapeDecoration(
-              color: color,
-              shape: const CircleBorder(),
-            ),
+        for (int i = 0; i < 3; i++)
+          AnimatedBuilder(
+            animation: animations[i],
+            builder: (context, child) {
+              return Opacity(
+                opacity: animations[i].value,
+                child: Container(
+                  width: 15.h,
+                  height: 15.h,
+                  decoration: ShapeDecoration(
+                    color: color,
+                    shape: const CircleBorder(),
+                  ),
+                ),
+              );
+            },
           ),
       ].separatedBy(8.horizontalSpace),
     );
