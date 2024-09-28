18d6c96c39a1e098a1829789b6b9a18e36f0e5ab
MaziVincent <georgevincent35@gmail.com>
2024-09-28 07:55:45 +0100

    baseurl

--- Web/LifePadiSPA/src/api/baseUrl.js
+++ Web/LifePadiSPA/src/api/baseUrl.js
@@ -20,6 +20,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 //const baseUrl = "https://4i2f5way3slxupj3rkqood3wje0txtca.lambda-url.us-east-1.on.aws/api/"
 const baseUrl = "https://lifepadi-948405839190.us-east1.run.app/api/";
 //const baseUrl ='https://localhost:7216/api/'
@@ -65,6 +66,8 @@ const baseUrl = "https://4i2f5way3slxupj3rkqood3wje0txtca.lambda-url.us-east-1.o
 >>>>>>> 55c2e59 (baseurl)
 =======
 >>>>>>> 0ab5359 (incremental changes)
+=======
+>>>>>>> f28e588 (baseurl)
 const baseUrl = "https://localhost:7216/api/"
 =======
 <<<<<<< HEAD
@@ -137,8 +140,11 @@ const baseUrl = "https://4i2f5way3slxupj3rkqood3wje0txtca.lambda-url.us-east-1.o
 =======
 //const baseUrl = "https://4i2f5way3slxupj3rkqood3wje0txtca.lambda-url.us-east-1.on.aws/api/"
 >>>>>>> d523c13 (incremental changes)
+=======
+const baseUrl = "https://4i2f5way3slxupj3rkqood3wje0txtca.lambda-url.us-east-1.on.aws/api/"
+>>>>>>> 4baa364 (baseurl)
 //"https://4i2f5way3slxupj3rkqood3wje0txtca.lambda-url.us-east-1.on.aws/api/";
-const baseUrl ='https://localhost:7216/api/'
+//const baseUrl ='https://localhost:7216/api/'
 //'http://localhost:3500/'
 >>>>>>> 0ab4b1c (Google Maps Controller)
 
