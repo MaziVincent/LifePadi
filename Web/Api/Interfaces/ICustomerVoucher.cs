acaa3fef3deca83ff745d544f79ee5ad5180aa31
charlykso <charlykso141@gmail.com>
2024-10-01 11:54:57 +0100

    a little change in customer voucher service

--- Web/Api/Interfaces/ICustomerVoucher.cs
+++ Web/Api/Interfaces/ICustomerVoucher.cs
@@ -9,8 +9,8 @@ namespace Api.Interfaces
 {
     public interface ICustomerVoucher
     {
-        public Task<CustomerVoucherDto> CreateAsync(CustomerVoucherDto customerVoucher);
-        public Task<CustomerVoucherDto> UpdateAsync(CustomerVoucherDto customerVoucher, int id);
+        public Task<CustomerVoucherDto> CreateAsync(CustomerVoucherDto customerVoucherDto);
+        public Task<CustomerVoucherDto> UpdateAsync(CustomerVoucherDto customerVoucherDto, int id);
         public Task<string> DeleteAsync(int id);
         public Task<CustomerVoucherDto> GetAsync(int id);
         public Task<IEnumerable<CustomerVoucherDto>> AllAsync();
