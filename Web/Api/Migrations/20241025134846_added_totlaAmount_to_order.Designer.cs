e9e1aef08d32b22edfe7bf5c4c63d3d7a523bcab
charlykso <charlykso141@gmail.com>
2024-10-25 16:14:47 +0100

    added total amount to order model

--- Web/Api/Migrations/20241025134846_added_totlaAmount_to_order.Designer.cs
+++ Web/Api/Migrations/20241025134846_added_totlaAmount_to_order.Designer.cs
@@ -4,6 +4,7 @@ using System.Numerics;
 using Api.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Api.Migrations
 {
     [DbContext(typeof(DBContext))]
-    partial class DBContextModelSnapshot : ModelSnapshot
+    [Migration("20241025134846_added_totlaAmount_to_order")]
+    partial class added_totlaAmount_to_order
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -764,9 +767,6 @@ namespace Api.Migrations
 
                     b.Property<string>("Description")
                         .HasColumnType("text");
-
-                    b.Property<double?>("DiscountAmount")
-                        .HasColumnType("double precision");
 
                     b.Property<int?>("DiscountPercentage")
                         .HasColumnType("integer");
