dbda37fdc6566183a8c38e3b184b65cb8ea07462
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-11-26 06:22:09 +0100

    Implement logout for riders

--- mobile/lifepadi/lib/pages/rider_page.dart
+++ mobile/lifepadi/lib/pages/rider_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:iconsax_plus/iconsax_plus.dart';
 import 'package:lifepadi/models/order.dart';
+import 'package:lifepadi/router/routes.dart';
+import 'package:lifepadi/state/auth_controller.dart';
 import 'package:lifepadi/utils/constants.dart';
+import 'package:lifepadi/utils/helpers.dart';
 import 'package:lifepadi/widgets/widgets.dart';
 
 class RiderPage extends StatelessWidget {
@@ -16,10 +21,31 @@ class RiderPage extends StatelessWidget {
           title: 'Deliveries',
           height: 126.h,
           actions: [
-            /// logout button
-            MyIconButton(
-              icon: IconsaxPlusLinear.logout,
-              onPressed: () {},
+            /// Logout button
+            Consumer(
+              builder: (context, ref, child) {
+                return MyIconButton(
+                  icon: IconsaxPlusLinear.logout,
+                  onPressed: () async {
+                    await openChoiceDialog(
+                      context: context,
+                      title: 'Logout of Lifepadi?',
+                      description: 'Are you sure you want to log out?',
+                      icon: IconsaxPlusLinear.logout,
+                      onYes: () async {
+                        await ref
+                            .read(authControllerProvider.notifier)
+                            .logout()
+                            .then((_) {
+                          if (context.mounted) {
+                            context.go(const LoginRoute().location);
+                          }
+                        });
+                      },
+                    );
+                  },
+                );
+              },
             ),
           ],
           bottom: TabBar(
