e45def29114d25e0abedd11096acd20a0e706215
Sixtus Agbo <miracleagbosixtus@gmail.com>
2025-02-12 23:17:13 +0100

    Implement APNS

--- mobile/lifepadi/lib/main.dart
+++ mobile/lifepadi/lib/main.dart
@@ -13,6 +13,10 @@ import 'utils/state_logger.dart';
 
 @pragma('vm:entry-point')
 Future<void> _handleFcmBackgroundMessage(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
   if (message.notification == null) return;
   final route = message.data['route'] as String?;
 
