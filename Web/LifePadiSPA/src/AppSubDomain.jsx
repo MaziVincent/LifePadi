75a1d84d8318d711f81546d8ca5035d625a00ef3
MaziVincent <georgevincent35@gmail.com>
2025-05-05 19:07:29 +0100

    keep modifying the subdomain

--- Web/LifePadiSPA/src/AppSubDomain.jsx
+++ Web/LifePadiSPA/src/AppSubDomain.jsx
@@ -8,14 +8,6 @@ const AppSubdomain = () => {
 		const navigate = useNavigate();
 		const params = new URLSearchParams(location.search);
 	const reference = params.get("reference");
-	
-	// useEffect(() => {
-	// 	const handleRedirect = async () => { 
-	// 		window.location.href = `https://app.lifepadi.com/payment/confirm?reference=${reference}`;
-	// 	}
-
-		
-	// }, [reference]);
     
 
 
@@ -23,21 +15,12 @@ const AppSubdomain = () => {
 		<div className="flex flex-col items-center justify-center gap-5 h-screen">
 			<h1 className="text-2xl">Payment Processing... </h1>
 			<p className="flex flex-col items-center">
-				<button
-					onClick={() => (window.location.href = `https://app.lifepadi.com/payment/confirm?reference=${reference}`)}
+				<a
+					href={`https://app.lifepadi.com/payment/confirm?reference=${reference}`}
 					className="p-3 bg-secondary text-white rounded-md cursor-pointer"
 				>
-					Open in App
-				</button>
-{/* 
-				<div className="flex justify-center gap-6">
-					<a className="bg-secondary p-3" href="https://play.google.com/store/apps/details?id=com.lifepadi.app">
-						Download App (Android){" "}
-					</a>
-					<a className="bg-secondary p-3 " href="https://apps.apple.com/us/app/lifepadi/id6741829265">
-						Download App (IOS){" "}
-					</a>
-				</div> */}
+					Continue 
+				</a>
 			</p>
 		</div>
 	);
