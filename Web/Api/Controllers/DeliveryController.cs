7f426bd32300e08a76e9ea0f7c2ee544fbc393a0
MaziVincent <georgevincent35@gmail.com>
2025-07-02 15:32:20 +0100

    delivery controller

--- Web/Api/Controllers/DeliveryController.cs
+++ Web/Api/Controllers/DeliveryController.cs
@@ -307,7 +307,7 @@ namespace Api.Controllers
             // Parse price per kilometer with fallback
             if (!int.TryParse(_config.GetSection("Distance:Price_Per_Kilometer").Value, out int pricePerKilometer))
             {
-                pricePerKilometer = 100; // Default price per kilometer
+                pricePerKilometer = 300; // Default price per kilometer
             }
 
             if (delivery.DiscountPercentage != null && delivery.DiscountAmount != null)
